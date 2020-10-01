356972cf422e6dfb789f5abb58e4fef664d81bb3
matt <anderson_rayner@hotmail.com>
2020-09-30 22:46:01 -0700

    stuff happening

--- functions/find_white_space.m
+++ functions/find_white_space.m
@@ -1,14 +1,34 @@
-function idx = find_white_space(image_matrix)
-    for ii = 1:size(image_matrix,1)
-        for jj = 1:size(image_matrix,2)
-            if image_matrix(ii,jj) > 
-            
+function [x,y] = find_white_space(image_matrix, startX, startY)
+% Returns the first index of a white pixel
+
+startY = 1;
+
+% Loop through each pixel in the image
+for ii = startX:size(image_matrix,1)
+    for jj = startY:size(image_matrix,2)
+        % Check for white pixel 
+        if image_matrix(ii,jj)
+            % Return the image
+%              fprintf('White found at (%d, %d)\n',ii,jj);
+             x = ii;
+             y = jj;
+             
+             % Expand the white space out
+             x = [ii, ii+1];
+             y = [jj];
+             
+             
+            return
             
         end
     end
+    fprintf('%5d of %5d\n',ii,size(image_matrix,1));
+end
+    
+% If we got this far, there are no whites left
+x = nan;
+y = nan;
+return
 
+% function check_pixel_neighbours(x,y,maxX,maxY)
 
-
-
-
-return 1
--- main.m
+++ main.m
@@ -5,21 +5,59 @@ close all
 clc
 
 addpath('./functions/');
+dbstop if error
 
 
+% file2 = './images/24bit.bmp';
 file = './images/monochrome.bmp';
+file = './images/mono_simple.bmp';
 
 %% Import Image
 imdata = imread(file);
+imdata_original = imdata;
 
 %% Create output image
-im_out = zeros(size(imdata));
+im_out = uint8(zeros([size(imdata),3]));
+
+im_out(:,:,1) = imdata*256;
+im_out(:,:,2) = imdata*256;
+im_out(:,:,3) = imdata*256;
+
+x = 1;
+y = 1;
+
+red = 1;
+green = 50;
+blue = 125;
+
+while ~isnan(x)
+    % Find the white space
+    [x,y] = find_white_space(imdata,x,y);
+    
+    % Colour the white space in
+    if ~isnan(x)
+%         red = red + 1;     if red > 255; red = 0; end
+%         green = green + 1; if green > 255; green = 0; end
+%         blue = blue + 1;   if blue > 255; blue = 0; end
+        
+        red = rand(1) * 255;
+        green = rand(1) * 255;
+        blue = rand(1) * 255;
+              
+        
+        im_out(x,y,1) = red;
+        im_out(x,y,2) = green;
+        im_out(x,y,3) = blue;
+%         keyboard
+        imdata(x,y) = 0;
+    end
+end
 
 
 %% Plots
 figure(1); clf; hold all; set(gcf,'name','Matt and Jen''s Colouring!');
 subplot(1,2,1); ...
-    imshow(imdata); ...
+    imshow(imdata_original); ...
     title('Original');
 
 subplot(1,2,2); ...
