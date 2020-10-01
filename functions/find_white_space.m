85a8f79a461a57fe9560a725962260b903af7e8c
matt <anderson_rayner@hotmail.com>
2020-09-30 23:02:17 -0700

    still not working...

--- functions/find_white_space.m
+++ functions/find_white_space.m
@@ -6,29 +6,51 @@ startY = 1;
 % Loop through each pixel in the image
 for ii = startX:size(image_matrix,1)
     for jj = startY:size(image_matrix,2)
-        % Check for white pixel 
+        % Check for white pixel
         if image_matrix(ii,jj)
-            % Return the image
-%              fprintf('White found at (%d, %d)\n',ii,jj);
-             x = ii;
-             y = jj;
-             
-             % Expand the white space out
-             x = [ii, ii+1];
-             y = [jj];
-             
-             
+            %              fprintf('White found at (%d, %d)\n',ii,jj);
+            x = ii;
+            y = jj;
+            
+            [ ~,x,y ] = expand_and_mark(image_matrix,x,y);
+            keyboard
+            % Expand the white space out
+            
+            
+            
             return
             
         end
     end
     fprintf('%5d of %5d\n',ii,size(image_matrix,1));
 end
-    
+
 % If we got this far, there are no whites left
 x = nan;
 y = nan;
 return
+end
 
-% function check_pixel_neighbours(x,y,maxX,maxY)
+function [image_matrix_new,x_new,y_new] = expand_and_mark(image_matrix,x,y)
+
+fprintf('Checking (%d,%d) : %d\n' ,...
+    x(end),y(end),image_matrix(x(end),y(end)));
+image_matrix(x(end),y(end)) = 0;
+
+if (image_matrix(x+1,y))
+    fprintf('\tExpanding\n');
+    x = [x, x+1];
+    y = [y, y];
+    [image_matrix_new,x_new,y_new] = expand_and_mark(image_matrix,x,y);
+end
+return
+end
+
+
+
+
+
+
+
+
 
