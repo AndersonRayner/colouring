93c092ee4d46a86d6e32b4ed10ae3160f7293421
Matt Anderson <anderson_rayner@hotmail.com>
2020-10-05 02:36:46 -0700

    Sped the system up significantly by marking as process when found

--- matt/functions/find_white_space.m
+++ matt/functions/find_white_space.m
@@ -24,42 +24,44 @@ while (Xpos <=  Xmax)
             newY(end+1) = Ypos;
             
             while (~isempty(newX))
-                % Check the pixel is white, and if so, process
+                % Pixel should be white, look around it
                 Xpos = newX(1);
                 Ypos = newY(1);
                 
-                if (image_matrix(Xpos,Ypos))
-                    
-                    % Add pixel to our output indicies
-                    idxX(end+1) = Xpos;
-                    idxY(end+1) = Ypos;
-                    
-                    % Expand out the found pixel
-                    % Look up
-                    if image_matrix(Xpos,max(Ypos-1,1))
-                        newX(end+1) = Xpos;
-                        newY(end+1) = max(Ypos-1,1);
-                    end
-                    
-                    % Look left
-                    if image_matrix(max(Xpos-1,1),Ypos)
-                        newX(end+1) = max(Xpos-1,1);
-                        newY(end+1) = Ypos;
-                    end
-                    
-                    % Look right
-                    if image_matrix(min(Xpos+1,Xmax),Ypos)
-                        newX(end+1) = min(Xpos+1,Xmax);
-                        newY(end+1) = Ypos;
-                    end
-                    
-                    % Look down
-                    if image_matrix(Xpos,min(Ypos+1,Ymax))
-                        newX(end+1) = Xpos;
-                        newY(end+1) = min(Ypos+1,Ymax);
-                    end
-                    
+                % Add current pixel to our output indicies
+                idxX(end+1) = Xpos;
+                idxY(end+1) = Ypos;
+                
+                % Expand out the found pixel
+                % Look up
+                if image_matrix(Xpos,max(Ypos-1,1))
+                    newX(end+1) = Xpos;
+                    newY(end+1) = max(Ypos-1,1);
+                    image_matrix(Xpos,max(Ypos-1,1)) = 0;
                 end
+                
+                % Look left
+                if image_matrix(max(Xpos-1,1),Ypos)
+                    newX(end+1) = max(Xpos-1,1);
+                    newY(end+1) = Ypos;
+                    image_matrix(max(Xpos-1,1),Ypos) = 0;
+                end
+                
+                % Look right
+                if image_matrix(min(Xpos+1,Xmax),Ypos)
+                    newX(end+1) = min(Xpos+1,Xmax);
+                    newY(end+1) = Ypos;
+                    image_matrix(min(Xpos+1,Xmax),Ypos) = 0;
+                end
+                
+                % Look down
+                if image_matrix(Xpos,min(Ypos+1,Ymax))
+                    newX(end+1) = Xpos;
+                    newY(end+1) = min(Ypos+1,Ymax);
+                    image_matrix(Xpos,min(Ypos+1,Ymax)) = 0;
+                end
+                
+                
                 
                 % Mark pixel as indexed
                 image_matrix(Xpos,Ypos) = 0;
